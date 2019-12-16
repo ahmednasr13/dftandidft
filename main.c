a048dba1e8cb4f8c122265d213a07d9b9cb3219e
ahmednasr13 <51250388+ahmednasr13@users.noreply.github.com>
2019-12-16 18:17:18 +0200

    Main file

--- main.c
+++ main.c
@@ -1,15 +1,7 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <math.h>
 
-typedef int Int16;
+typedef short int Int16;
 
 typedef struct
 {
@@ -18,8 +10,14 @@ typedef struct
 } COMPLEX;
 
 #define BUFFSIZE 1000
-#define PI 3.14159265358979 
-#define N 100
+
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
+
+#define N 100		// N-Point DFT
+
+#define NUMBER_OF_PRINTED_ELEMENTS 10
 
 void dft(Int16 *inbuf, Int16 length);
 
@@ -28,20 +26,23 @@ Int16 res_idft[BUFFSIZE];
 
 int main()
 {
-    Int16 input[1000] = {[0] = 10, [1] = 10, [2] = 10};
+    Int16 input[1000] = {[0] = 21, [1] = 523, [2] = 12};
     dft(input, BUFFSIZE);
     
-    int i;
-    for(i=0;i<10;i++)
+    Int16 i;
+    printf("Input = \n");
+    for(i=0;i<NUMBER_OF_PRINTED_ELEMENTS;i++)
         printf("%d ", input[i]);
     printf("\n");
-    for(i=0;i<10;i++)
+    
+    printf("Absolute of DFT = \n");
+    for(i=0;i<NUMBER_OF_PRINTED_ELEMENTS;i++)
         printf("%d ", res_dft[i]);
     printf("\n");
-    for(i=0;i<10;i++)
-        printf("%d ", res_idft[i]);
     
-    //printf("\n%d", res_dft[429]);
+    printf("Absoulte of IDFT = \n");
+    for(i=0;i<NUMBER_OF_PRINTED_ELEMENTS;i++)
+        printf("%d ", res_idft[i]);
 
     return 0;
 }
@@ -50,44 +51,37 @@ void dft(Int16 *inbuf, Int16 length)
 {
 	COMPLEX result[BUFFSIZE];
 	COMPLEX res_inv[BUFFSIZE];
-    Int16 k, n;
+	Int16 k, n;
 	
 	for (k = 0; k < length; k++)
 	{ 
-        result[k].real = 0.0;    
+        	result[k].real = 0.0;    
 		result[k].imag = 0.0;
 		
 		for (n = 0; n < N; n++)
-	    {
-		    //result[k].real += inbuf[n].real*cos(2*PI*k*n/N) + inbuf[n].imag*sin(2*PI*k*n/N); 
-			result[k].real += inbuf[n] * cos(2*PI*k*n/N);
+	    	{
+			//result[k].real += inbuf[n].real*cos(2*M_PI*k*n/N) + inbuf[n].imag*sin(2*M_PI*k*n/N); 
+			result[k].real += inbuf[n] * cos(2*M_PI*k*n/N);
 			
-			//result[k].imag += inbuf[n].imag*cos(2*PI*k*n/N) - inbuf[n].real*sin(2*PI*k*n/N);  
-			result[k].imag -= inbuf[n] * sin(2*PI*k*n/N);
+			//result[k].imag += inbuf[n].imag*cos(2*M_PI*k*n/N) - inbuf[n].real*sin(2*M_PI*k*n/N);  
+			result[k].imag -= inbuf[n] * sin(2*M_PI*k*n/N);
 		}
 		
-		res_dft[k] = sqrt(result[k].real*result[k].real + result[k].imag*result[k].imag);
+		res_dft[k] = roundf(sqrt(result[k].real*result[k].real + result[k].imag*result[k].imag));
 	}
 		
 	for (n = 0; n < length; n++)
-    {
-        res_inv[n].real = 0.0;    
+    	{
+        	res_inv[n].real = 0.0;    
 		res_inv[n].imag = 0.0;
 		
 		for (k = 0; k < N; k++)
-	    {
-			res_inv[n].real += (result[k].real * cos(2*PI*k*n/N) - result[k].imag * sin(2*PI*k*n/N)) / N;
+	    	{
+			res_inv[n].real += (result[k].real * cos(2*M_PI*k*n/N) - result[k].imag * sin(2*M_PI*k*n/N)) / N;
 			
-			res_inv[n].imag += (result[k].real * sin(2*PI*k*n/N) + result[k].imag * cos(2*PI*k*n/N)) / N; 			 
+			res_inv[n].imag += (result[k].real * sin(2*M_PI*k*n/N) + result[k].imag * cos(2*M_PI*k*n/N)) / N; 			 
 		}
 		
-		res_idft[n] = sqrt(res_inv[n].real*res_inv[n].real + res_inv[n].imag*res_inv[n].imag);
+		res_idft[n] = roundf(sqrt(res_inv[n].real*res_inv[n].real + res_inv[n].imag*res_inv[n].imag));
 	}
-	
-	for (k=0; k<10; k++)
-	    if (res_inv[k].imag >= 0)
-	        printf("%.4f+%.4fj ", res_inv[k].real, res_inv[k].imag);
-	    else
-	        printf("%.4f%.4fj ", res_inv[k].real, res_inv[k].imag);
-	printf("\n");
 }
